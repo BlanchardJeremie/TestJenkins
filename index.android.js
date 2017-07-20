318b4af49c47656dab5a0c9a350b02947ebe765f
Blanchard Jérémie <jeremie.blanchard@outlook.fr>
2017-07-20 11:19:36 +0200

    first commit

---
+++ index.android.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+import NavigationContainer from './src/NavigationContainer';
+
+export default class ReactNativeSample extends Component {
+  render() {
+    return (
+      <NavigationContainer />
+    );
+  }
+}
+
+AppRegistry.registerComponent('ReactNativeSample', () => ReactNativeSample);
